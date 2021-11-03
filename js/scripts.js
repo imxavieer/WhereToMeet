b06feb4505f3f18d2273eda2fe90091fcc04677b
bryan88844 <ongzsbryan@gmail.com>
2021-11-03 21:30:03 +0800

    updated recommend function

--- js/scripts.js
+++ js/scripts.js
@@ -43,6 +43,9 @@ function initMap() {
 
 var total_lat = 0
 var total_lng = 0
+var avg_lat = 0
+var avg_lng = 0
+var recommended_address = ""
 
 function getLoc() {
     var addr = encodeURI(document.getElementById("addr").value);
@@ -119,10 +122,11 @@ function deleteItem(obj) {
 }
 
 function reverseGeo() {
-    var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + total_lat + "," + total_lng + "&key=AIzaSyAIoAdg46VQtDiLA1mU-aEXQrGtrkFrcqk";
+    var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + avg_lat + "," + avg_lng + "&key=AIzaSyAIoAdg46VQtDiLA1mU-aEXQrGtrkFrcqk";
     axios.get(url)
         .then(response => {   
-            document.getElementById("display").innerHTML = response.data.results[5].formatted_address;
+            recommended_address = document.getElementById("display").innerHTML = response.data.results[0].formatted_address;
+            
         })
         .catch(error => {
             console.log(error);
@@ -131,10 +135,11 @@ function reverseGeo() {
 }
 
 function recommend() {
-    total_lat /= num_of_inputs;
-    total_lng /= num_of_inputs;
-    document.getElementById('lat').value = total_lat;
-    document.getElementById('lng').value = total_lng;
+    console.log(total_lat,total_lng,num_of_inputs)
+    avg_lat = total_lat / num_of_inputs;
+    avg_lng = total_lng / num_of_inputs;
+    document.getElementById('lat').value = avg_lat;
+    document.getElementById('lng').value = avg_lng;
     initMap();
     reverseGeo();
 }
